3b80a0b8cff9062429763cf18d959f4d19dd32c2
Wiktor Religo <wiciorny@gmail.com>
2020-10-09 23:02:24 +0200

    promise all

--- JavaCodingChallanges/src/main/java/com/jadevirek/jsScopingAndFields.js
+++ JavaCodingChallanges/src/main/java/com/jadevirek/jsScopingAndFields.js
@@ -20,3 +20,42 @@ const getRandomUsers = n => {
 }
 
 getRandomUsers(100);
+
+
+/**
+ * PROMISE ALL
+ */
+
+
+const namesPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(['Anna', 'Jones', 'Ali', 'Jake']);
+    }, 3000);
+
+    setTimeout(() => {
+        reject("no data back from the server, there was an error");
+    }, 5000);
+});
+
+const surnamesPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(['Williams', 'Bravo', 'Mohammado', 'Smith']);
+    }, 3000);
+
+    setTimeout(() => {
+        reject("no data back from the server, there was an error");
+    }, 5000);
+});
+
+Promise.all([namesPromise, surnamesPromise]).then(data => {
+    const [names, surnames] = data;
+    for (var i = 0; i < names.length; i++) {
+        const name = names[i];
+        const surname = surnames[i];
+        log(`${name} ${surname}`);
+    }
+
+}).catch(error => {
+    log(error);
+});
+
