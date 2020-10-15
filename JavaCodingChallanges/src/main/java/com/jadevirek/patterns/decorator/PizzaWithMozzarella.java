b94208998b18c73c148bbe550318ca1d3e28242b
Wiktor Religo <wiciorny@gmail.com>
2020-10-15 23:05:33 +0200

    adding decorator example - not finished

---
+++ JavaCodingChallanges/src/main/java/com/jadevirek/patterns/decorator/PizzaWithMozzarella.java
@@ -0,0 +1,17 @@
+package main.java.com.jadevirek.patterns.decorator;
+
+import java.math.BigDecimal;
+
+public class PizzaWithMozzarella extends PizzaBase {
+    private static final BigDecimal MOZZARELLA_PRICE = new BigDecimal(5);
+    private final PizzaBase basePizza;
+
+    public PizzaWithMozzarella(PizzaBase basePizza) {
+        this.basePizza = basePizza;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return basePizza.getPrice().add(MOZZARELLA_PRICE);
+    }
+}
