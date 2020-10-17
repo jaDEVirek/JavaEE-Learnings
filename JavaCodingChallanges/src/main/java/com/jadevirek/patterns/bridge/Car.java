05d678a2104c9a3cb122c033ff29a6cd20cac802
Wiktor Religo <wiciorny@gmail.com>
2020-10-17 23:00:32 +0200

    implementing bridge pattern

---
+++ JavaCodingChallanges/src/main/java/com/jadevirek/patterns/bridge/Car.java
@@ -0,0 +1,15 @@
+package main.java.com.jadevirek.patterns.bridge;
+
+// Refine abstraction 1 in bridge pattern
+class Car extends Vehicle {
+    public Car(IWorkShop w1, IWorkShop w2) {
+        super(w1, w2);
+    }
+
+    @Override public void manufacture() {
+        System.out.print("Car ");
+        w1.work();
+        w2.work();
+    }
+
+}
---
+++ JavaCodingChallanges/src/main/java/com/jadevirek/patterns/bridge/IWorkShop.java
@@ -0,0 +1,7 @@
+package main.java.com.jadevirek.patterns.bridge;
+
+//Implementor for bridge pattern
+interface IWorkShop
+{
+    abstract public void work();
+}
